33b748b3c86aac7472a06233bb85dbf590fb4306
roctbb <roctbb@gmail.com>
2023-02-06 21:20:25 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ sudo cp agents_covid.conf /etc/supervisor/conf.d/
 sudo cp agents_covid_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
 sudo systemctl restart nginx
-sudo certbot --nginx -d covid.medsenger.ru
+sudo certbot --nginx -d covid.ai.medsenger.ru
